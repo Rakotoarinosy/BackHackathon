a54b7e9184c96b35b798edb5fd00e3814ed23e03
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-02-10 22:39:39 +0300

    routes

--- app.js
+++ app.js
@@ -2,17 +2,42 @@ const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
 require('dotenv').config();
+const cors = require('cors')
+const io = require('socket.io')(8081, {
+  cors: {
+      origin: 'http://localhost:3000',
+  }
+});
+
+
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(cors())
+
+
+
+const userRouter=require('./routes/user');
+const authRouter=require('./routes/auth');
+const controlleurRouter=require('./routes/controlleur');
+const receverRouter=require('./routes/receveur');
+
+
+
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
 
-app.use('/api', require('./routes/api.route'));
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
+app.use('/controlleur', controlleurRouter);
+app.use('/receveur', receveurRouter);
+
+
+
 
 app.use((req, res, next) => {
   next(createError.NotFound());
@@ -26,5 +51,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
